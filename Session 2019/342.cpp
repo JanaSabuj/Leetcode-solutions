7003ea915eb877c07ad919c84eab53be41b4ac62
Sabuj <sabujlovescoding@gmail.com>
2019-10-15 14:38:22 +0530

    power of 4.

---
+++ Session 2019/342.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if(n<1)
+            return false;
+        
+        while(n%4==0){
+            n/=4;
+        }
+        
+        if(n==1)
+            return true;
+        else
+            return false;
+    }
+};
