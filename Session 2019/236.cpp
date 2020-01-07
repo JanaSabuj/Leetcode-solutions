60effd80d36a48522a322623f1e5a9e9d3fa28be
Sabuj <sabujlovescoding@gmail.com>
2020-01-07 15:16:20 +0530

    LCA - recursive.

---
+++ Session 2019/236.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+typedef struct TreeNode node;
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root == NULL)
+            return NULL;
+        
+        if(root == p or root == q)
+            return root;
+        
+        node* left = lowestCommonAncestor(root->left, p, q);
+        node* right = lowestCommonAncestor(root->right, p, q);
+        
+        if(left and right)
+            return root;
+        else
+            return (left? left : right);
+    }
+};
