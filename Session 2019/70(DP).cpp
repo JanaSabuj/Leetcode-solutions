9dc5b344c02a1a420fcf1352fd37ce7294d424b2
Sabuj <sabujlovescoding@gmail.com>
2020-01-13 18:13:19 +0530

    climbing stairs.

--- Session 2019/70(DP).cpp
+++ Session 2019/70(DP).cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        int dp[n+1];
-     
-        for(int i=1; i<=n;i++){
-            if(i==1)
-                dp[i]=1;
-            else if(i==2)
-                dp[i]=2;
-            else
-                dp[i]=dp[i-1]+dp[i-2];
+        int dp[n + 1];
+        if(n == 1 or n == 2)
+            return n;
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i = 3; i <= n; i++){
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
-            
         
         return dp[n];
+        
     }
 };
