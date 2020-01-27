138b07c0cf2b5cfccaea6400ce3c835c519264d5
Sabuj <sabujlovescoding@gmail.com>
2020-01-27 23:36:23 +0530

    ccjusl.

---
+++ Session 2019/abc.cpp
@@ -0,0 +1,66 @@
+/*--------------------------"SABUJ-JANA"------"JADAVPUR UNIVERSITY"--------*/
+/*-------------------------------@greenindia-----------------------------------*/
+//
+//		  _____       _           _        _
+//		 / ____|     | |         (_)      | |
+//		| (___   __ _| |__  _   _ _       | | __ _ _ __   __ _
+//		 \___ \ / _` | '_ \| | | | |  _   | |/ _` | '_ \ / _` |
+//		 ____) | (_| | |_) | |_| | | | |__| | (_| | | | | (_| |
+//		|_____/ \__,_|_.__/ \__,_| |  \____/ \__,_|_| |_|\__,_|
+//		                        _/ |
+//		                       |__/
+/*---------------------- Magic. Do not touch.-----------------------------*/
+/*------------------------------God is Great/\---------------------------------*/
+#include <bits/stdc++.h>
+using namespace std;
+#define crap ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+//cout<<fixed<<showpoint<<setprecision(12)<<ans<<endl;
+#define int long long int
+#define double long double
+#define PI acos(-1)
+
+void print1d(const vector<int>& vec) {for (auto val : vec) {cout << val << " ";} cout << endl;}
+
+void print2d(const vector<vector<int>>& vec) {for (auto row : vec) {for (auto val : row) {cout << val << " ";} cout << endl;}}
+
+signed main() {
+
+	// freopen("input.txt", "r", stdin);
+	crap;
+
+    int n,m;
+    cin>>n>>m;
+
+    int A[n][m];
+    int B[n][m];
+
+    vector<pair<int,int>> vec;
+
+    for (int i = 0; i < n; ++i)
+    {
+    	/* code */
+
+    	for(int j=0; j<m; j++){
+    		cin>>A[i][j];
+    	}
+    }
+
+    for (int i = 0; i < n; ++i)
+    {
+    	/* code */
+
+    	for(int j=0; j<m; j++){
+    		cin>>B[i][j];
+    		if(B[i][j] > 0)
+    			vec.push_back({i,j});
+    	}
+    }
+
+    
+
+	return 0;
+}
+
+
+
+
