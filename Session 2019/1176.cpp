471b9d70287b7310283b0237231ac36d8fa582fa
Sabuj <sabujlovescoding@gmail.com>
2020-01-28 15:13:32 +0530

    Diet Plan Performance. Sliding Window.

---
+++ Session 2019/1176.cpp
@@ -0,0 +1,122 @@
+1176. Diet Plan Performance
+A dieter consumes calories[i] calories on the i-th day. 
+
+Given an integer k, for every consecutive sequence of k days (calories[i], calories[i+1], ..., calories[i+k-1] for all 0 <= i <= n-k), they look at T, the total calories consumed during that sequence of k days (calories[i] + calories[i+1] + ... + calories[i+k-1]):
+
+If T < lower, they performed poorly on their diet and lose 1 point; 
+If T > upper, they performed well on their diet and gain 1 point;
+Otherwise, they performed normally and there is no change in points.
+Initially, the dieter has zero points. Return the total number of points the dieter has after dieting for calories.length days.
+
+Note that the total points can be negative.
+
+ 
+
+Example 1:
+
+Input: calories = [1,2,3,4,5], k = 1, lower = 3, upper = 3
+Output: 0
+Explanation: Since k = 1, we consider each element of the array separately and compare it to lower and upper.
+calories[0] and calories[1] are less than lower so 2 points are lost.
+calories[3] and calories[4] are greater than upper so 2 points are gained.
+Example 2:
+
+Input: calories = [3,2], k = 2, lower = 0, upper = 1
+Output: 1
+Explanation: Since k = 2, we consider subarrays of length 2.
+calories[0] + calories[1] > upper so 1 point is gained.
+Example 3:
+
+Input: calories = [6,5,0,0], k = 2, lower = 1, upper = 5
+Output: 0
+Explanation:
+calories[0] + calories[1] > upper so 1 point is gained.
+lower <= calories[1] + calories[2] <= upper so no change in points.
+calories[2] + calories[3] < lower so 1 point is lost.
+ 
+
+Constraints:
+
+1 <= k <= calories.length <= 10^5
+0 <= calories[i] <= 20000
+0 <= lower <= upper
+
+
+
+/*--------------------------"SABUJ-JANA"------"JADAVPUR UNIVERSITY"--------*/
+/*-------------------------------@greenindia-----------------------------------*/
+//
+//		  _____       _           _        _
+//		 / ____|     | |         (_)      | |
+//		| (___   __ _| |__  _   _ _       | | __ _ _ __   __ _
+//		 \___ \ / _` | '_ \| | | | |  _   | |/ _` | '_ \ / _` |
+//		 ____) | (_| | |_) | |_| | | | |__| | (_| | | | | (_| |
+//		|_____/ \__,_|_.__/ \__,_| |  \____/ \__,_|_| |_|\__,_|
+//		                        _/ |
+//		                       |__/
+/*---------------------- Magic. Do not touch.-----------------------------*/
+/*------------------------------God is Great/\---------------------------------*/
+#include <bits/stdc++.h>
+using namespace std;
+#define crap ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+//cout<<fixed<<showpoint<<setprecision(12)<<ans<<endl;
+#define int long long int
+#define double long double
+#define PI acos(-1)
+
+void print1d(const vector<int>& vec) {for (auto val : vec) {cout << val << " ";} cout << endl;}
+
+void print2d(const vector<vector<int>>& vec) {for (auto row : vec) {for (auto val : row) {cout << val << " ";} cout << endl;}}
+
+
+
+int DPP(const vector<int>& calories, int k, int lower, int upper) {
+	int sum = 0;
+	int pts = 0;
+
+	int n = calories.size();
+
+	for (int i = 0; i < k; ++i)
+	{
+		sum += calories[i];
+	}
+
+	if (sum > upper)
+		pts += 1;
+	else if (sum < lower)
+		pts -= 1;
+
+
+	for (int i = k; i < n; i++) {
+		sum += calories[i] - calories[i - k];
+		if (sum > upper)
+			pts += 1;
+		else if (sum < lower)
+			pts -= 1;
+	}
+
+	return pts;
+
+
+}
+
+
+signed main() {
+
+	freopen("input.txt", "r", stdin);
+	crap;
+	// vector<int> calories = {1, 2, 3, 4, 5};
+	vector<int> calories = {3,2};
+	int k = 2;
+	int lower = 0;
+	int upper = 1;
+
+	cout << DPP(calories, k, lower, upper) << endl;
+
+
+	return 0;
+}
+
+
+
+
