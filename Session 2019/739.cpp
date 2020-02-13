cb0b6a10add69aaa7cca98e4f2462fa2e0fbfe89
Sabuj <sabujlovescoding@gmail.com>
2020-02-13 23:25:06 +0530

    Daily Temperatures. NGE. Stack.

--- Session 2019/739.cpp
+++ Session 2019/739.cpp
@@ -1,27 +1,50 @@
+739. Daily Temperatures
+Medium
+
+2050
+
+65
+
+Add to List
+
+Share
+Given a list of daily temperatures T, return a list such that, for each day in the input, tells you how many days you would have to wait until a warmer temperature. If there is no future day for which this is possible, put 0 instead.
+
+For example, given the list of temperatures T = [73, 74, 75, 71, 69, 72, 76, 73], your output should be [1, 1, 4, 2, 1, 1, 0, 0].
+
+Note: The length of temperatures will be in the range [1, 30000]. Each temperature will be an integer in the range [30, 100].
+
+Accepted
+119,854
+Submissions
+193,558
+---------------------------------------------------------
 class Solution {
 public:
-	vector<int> dailyTemperatures(vector<int>& T) {
-		int n = T.size();
-		vector<int> dp(n);
+	vector<int> dailyTemperatures(vector<int>& arr) {
+		int n = arr.size();
+		vector<int> ans(n);
+		stack<pair<int, int>> s;
+		s.push({arr[0], 0});// (val,idx)
 
-		for (int i = n - 1; i >= 0; i--) {
-			if (i == n - 1)
-				dp[i] = 0;
-			else {
-				bool flag = false;
-				for (int j = i + 1; j < n; j++) {
-					if (T[j] > T[i]) {
-						dp[i] = j - i;
-						flag = true;
-						break;
-					}
-				}
-
-				if (!flag)
-					dp[i] = 0;
+		for (int i = 1; i < n; i++) {
+			if (s.empty()) {
+				s.push({arr[i], i});
+				continue;
 			}
+
+			while (!s.empty() and s.top().first < arr[i]) {
+				ans[s.top().second] = i - s.top().second;
+				s.pop();
+			}
+			s.push({arr[i], i});
 		}
 
-		return dp;
+		while (!s.empty()) {
+			ans[s.top().second] = 0;
+			s.pop();
+		}
+
+		return ans;
 	}
 };
