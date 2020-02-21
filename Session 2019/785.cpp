9c7173396d118870fc4c95ec70fc193dec072a53
Sabuj <sabujlovescoding@gmail.com>
2020-02-21 19:09:34 +0530

    Is Graph Bipartite

--- Session 2019/785.cpp
+++ Session 2019/785.cpp
@@ -1,6 +1,59 @@
 class Solution {
 public:
 
+	bool bfsUtil(const vector<vector<int>>& adj, int start, int color[]){
+
+		queue<int> q;
+		color[start] = 0;
+		q.push(start);
+
+		while(!q.empty()){
+			int u = q.front();
+			q.pop();
+
+			for(auto x: adj[u]){
+				if(color[x] == -1){
+					color[x] = !color[u];
+					q.push(x);
+				}else if(color[x] == color[u]){
+					return false;
+				}
+			}
+		}
+
+		return true;
+	}
+
+	bool bfs(const vector<vector<int>>& adj, int n){
+		int color[n];
+		memset(color, -1, sizeof(color));
+
+		for(int i = 0; i < n; i++){
+			if(color[i] == -1){
+				bool val = bfsUtil(adj, i, color);
+				if(!val) return false;
+			}
+		}
+
+		return true;
+	}
+
+    bool isBipartite(vector<vector<int>>& graph) {
+    	int n = graph.size();
+    	vector<vector<int>> adj(n);
+    	for(int i = 0; i < n; i++){
+    		// ith- node
+    		for(auto x: graph[i])
+    			adj[i].push_back(x);
+    	}
+    	
+    	return bfs(adj, n);                        
+    }
+};
+//----------------------------------------------------------------------------
+class Solution {
+public:
+
 	bool dfsUtil(const vector<vector<int>>& adj, int start, int color[], int c){
 		if(color[start] == 0)
 			color[start] = c;
