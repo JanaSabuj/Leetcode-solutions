88962d3973d71542868442ce7dda1dd5f0e38a91
Sabuj Jana <sabujlovescoding@gmail.com>
2020-03-01 17:50:47 +0530

    Update 452.cpp

--- Session 2019/452.cpp
+++ Session 2019/452.cpp
@@ -24,3 +24,34 @@ public:
         return cnt;
     }
 };
+
+// another soln
+class Solution {
+public:
+    static bool comp(const vector<int>& v1, const vector<int>& v2){
+        return v1[1] < v2[1];
+    }
+    
+    int findMinArrowShots(vector<vector<int>>& points) {
+        
+        int n = points.size();
+        if(n == 0)
+            return 0;
+        sort(points.begin(), points.end(), comp);
+        
+        int i = 0;
+        int cnt = 1;
+    
+        for(int j = 1; j < n; j++){
+            if(points[i][1] < points[j][0]){
+                // imp. <= will give extra ans bcoz the common edge-point 
+                // can burst both the balloons
+                //e.g -> 1,6 6,10 requires 1 arrow not 2. 
+                i = j;
+                cnt++;
+            }
+        }
+        
+        return cnt;
+    }
+};
