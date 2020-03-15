3faf04706a85a186d39905ab47f75dd9fb6c9b46
Sabuj Jana <sabujlovescoding@gmail.com>
2020-03-15 22:57:58 +0530

    Update in_out_dp_trees.cpp

--- Session 2019/DP/in_out_dp_trees.cpp
+++ Session 2019/DP/in_out_dp_trees.cpp
@@ -25,20 +25,47 @@ void print2d(const vector<vector<int>>& vec) {for (auto row : vec) {for (auto va
 
 const int N = 100007;
 vector<int> adj[N];
-int arr[N];
-int dp[N];
+int in[N];
+int out[N];
 
-void dfs(int u, int par){
-	dp[u] = arr[u - 1];// dp initial - root
+void dfs1(int u, int par) {
+	in[u] = 0;// root ht
 
-	int mx = 0;
-	for(auto child: adj[u]){
-		if(child == par) continue;
-		dfs(child, u);
-		mx = max(mx, dp[child]);
+	for (auto child : adj[u]) {
+		if (child == par) continue;
+		dfs1(child, u);
+		in[u] = max(in[u], 1 + in[child]);// we get in[child] if we have got it by dfs
 	}
 
-	dp[u] += mx;// dp updt - for rest
+	// in[u] = 1 + mx;// formula DONT KEEP IT OUTSIDE
+}
+
+void dfs2(int u, int par) {
+	int mx1 = -1;
+	int mx2 = -1;
+
+	// find mx1 and mx2 of in[siblings]
+	for (auto child : adj[u]) {
+		if (child == par) continue;
+
+		if (in[child] >= mx1 ) {
+			mx2 = mx1;
+			mx1 = in[child];
+		} else if (in[child] > mx2)
+			mx2 = in[child];
+	}
+
+	for(auto child: adj[u]){
+		if(child == par) continue;
+		int use = mx1;
+		if(mx1 == in[child])
+			use = mx2;
+
+		out[child] = max(2 + use, 1 + out[u]);	
+		dfs2(child, u);
+	}
+
+
 }
 
 signed main() {
@@ -47,10 +74,9 @@ signed main() {
 #endif
 	crap;
 
-	int n;// nodes
+	int n;
 	cin >> n;
 
-	// n-1 edges
 	for (int i = 0; i < n - 1; i++) {
 		int x, y;
 		cin >> x >> y;
@@ -59,14 +85,19 @@ signed main() {
 		adj[y].push_back(x);
 	}
 
+	memset(in,0,sizeof(in));
+	memset(out,0,sizeof(out));// out[root] = 0;
+	dfs1(1, 0);
+	dfs2(1, 0);
 
-	for(int i=0; i<n; i++){
-		cin>>arr[i];
+	int dp[n + 1];
+
+	for (int i = 1; i <= n; i++) {
+		dp[i] = max(in[i], out[i]);
+		cout << dp[i] << " ";
+		// cout << in[i] << " " << out[i] << " " << dp[i] << endl ;
 	}
 
-	// dfs called like node, par
-	dfs(1, 0);
-	cout << dp[1] << endl;
 	return 0;
 }
 
