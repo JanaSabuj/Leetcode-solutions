19fb4b9379973ceaaa56a8aae9a2306435265ef7
Sabuj Jana <sabujlovescoding@gmail.com>
2020-04-27 09:35:02 +0530

    Update 111(Min Depth of BT).cpp

--- Apr'20/111(Min Depth of BT).cpp
+++ Apr'20/111(Min Depth of BT).cpp
@@ -39,3 +39,29 @@ public:
         
     }
 };
+
+///////////////////
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if(root == NULL)
+            return 0;
+        if(root->left == NULL and root->right == NULL)
+            return 1;
+        if(root->left and root->right == NULL)
+            return 1 + minDepth(root->left);
+        if(root->right and root->left == NULL)
+            return 1 + minDepth(root->right);
+        return 1 + min(minDepth(root->left), minDepth(root->right));
+    }
+};
