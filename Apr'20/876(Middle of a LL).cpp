3d4dc1037a9cc1ce4b49833db9ebb988ae81ba5e
Sabuj Jana <sabujlovescoding@gmail.com>
2020-04-30 21:02:06 +0530

    Update 876(Middle of a LL).cpp

--- Apr'20/876(Middle of a LL).cpp
+++ Apr'20/876(Middle of a LL).cpp
@@ -33,3 +33,28 @@ public:
         return NULL;
     }
 };
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+typedef struct ListNode node;
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        node* p = head, *q = head;
+        
+        while(q and q->next){
+            p = p->next;
+            q = q->next->next;            
+        }
+        
+        return p;
+    }
+};
