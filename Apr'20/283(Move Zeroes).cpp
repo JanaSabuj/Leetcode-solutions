b7cf34f4ab362db4c82d69798be7950c284b2b8b
Sabuj Jana <sabujlovescoding@gmail.com>
2020-04-30 12:53:54 +0530

    Update 283(Move Zeroes).cpp

--- Apr'20/283(Move Zeroes).cpp
+++ Apr'20/283(Move Zeroes).cpp
@@ -15,3 +15,17 @@ public:
         }
     }
 };
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+        int nxt = 0;
+        for(int i = 0; i < n; i++){
+            if(nums[i] != 0){
+               swap(nums[i], nums[nxt++]);
+            }                
+        }
+       
+    }
+};
