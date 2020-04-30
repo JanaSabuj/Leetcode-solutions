e133e50b7be02bf6cbe2111e34467bd2abbcb872
Sabuj Jana <sabujlovescoding@gmail.com>
2020-04-30 20:14:13 +0530

    Create 876(Middle of a LL).cpp

---
+++ Apr'20/876(Middle of a LL).cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+typedef struct ListNode node;
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        int len = 0;
+        node* p = head;
+        while(p != NULL){
+            len++;
+            p = p->next;
+        }
+        
+        int middle = len/2 + 1;
+        
+        p = head;
+        int cnt = 0;
+        while(p != NULL){
+            cnt++;            
+            if(cnt == (middle))
+                return p;
+            p = p->next;
+        }
+        
+        return NULL;
+    }
+};
