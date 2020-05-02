3c398e528433e8962b7c71919e78240d300637fe
Sabuj Jana <sabujlovescoding@gmail.com>
2020-05-02 21:13:30 +0530

    Update 643(Maximum Average Subarray I).cpp

--- Apr'20/643(Maximum Average Subarray I).cpp
+++ Apr'20/643(Maximum Average Subarray I).cpp
@@ -20,3 +20,22 @@ public:
         return mx;
     }
 };
+------------------------------------
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        int n = nums.size();
+        
+        int sum = 0;
+        for(int i = 0; i < k; i++)
+            sum += nums[i];
+        
+        int mx = sum;
+        for(int i = k; i < n; i++){
+            sum += nums[i] - nums[i-k];
+            mx = max(mx, sum);
+        }
+        
+        return (double)mx/(double)k;
+    }
+};
