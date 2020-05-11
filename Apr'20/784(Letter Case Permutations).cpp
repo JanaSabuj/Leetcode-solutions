15bc9b8343dde49346ddf28043b976ffe48a4a60
Sabuj Jana <sabujlovescoding@gmail.com>
2020-05-11 18:02:03 +0530

    Create 784(Letter Case Permutations).cpp

---
+++ Apr'20/784(Letter Case Permutations).cpp
@@ -0,0 +1,36 @@
+class Solution {
+    string str;
+    int len;    
+    vector<string> vec;
+public:
+    
+    void dfs(int idx, string temp){
+        
+        if(idx == len){
+            vec.push_back(temp);
+            return;
+        }
+        
+        if(isalpha(str[idx])){
+            temp += tolower(str[idx]);
+            dfs(idx + 1, temp);
+            temp.pop_back();
+            
+            temp += toupper(str[idx]);
+            dfs(idx + 1, temp);
+            temp.pop_back();
+        }else{
+            temp += str[idx];
+            dfs(idx + 1, temp);
+        }
+    }
+    
+    vector<string> letterCasePermutation(string S) {
+        str = S;
+        len = S.length();        
+        vec.clear();
+        
+        dfs(0, "");
+        return vec;
+    }
+};
