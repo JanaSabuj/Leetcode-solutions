1d829b273a6a8587380856c06b44b204ef726d2f
Sabuj Jana <sabujlovescoding@gmail.com>
2020-05-11 19:04:00 +0530

    Update 784(Letter Case Permutations).cpp

--- Apr'20/784(Letter Case Permutations).cpp
+++ Apr'20/784(Letter Case Permutations).cpp
@@ -34,3 +34,48 @@ public:
         return vec;
     }
 };
+
+USING REFERENCE VARIABLES
+------------------------------
+    
+    class Solution {
+    string str;
+    int len;    
+    vector<string> vec;
+public:
+    
+    void dfs(int idx, string& temp){
+        
+        if(idx == len){
+            vec.push_back(temp);
+            return;
+        }
+        
+        if(isalpha(str[idx])){
+            temp += tolower(str[idx]);
+            dfs(idx + 1, temp);             
+            temp.pop_back();
+            
+            temp += toupper(str[idx]);
+            dfs(idx + 1, temp);
+            temp.pop_back();
+        }else{
+            temp += str[idx];
+            dfs(idx + 1, temp);  
+            temp.pop_back();
+        }
+        
+        return;
+    }
+    
+    vector<string> letterCasePermutation(string S) {
+        
+        str = S;
+        len = S.length();        
+        vec.clear();
+        
+        string ans = "";
+        dfs(0, ans);
+        return vec;
+    }
+};
