7ddfa620e7c9f52ac52b820b48e1c393df5fb07f
Sabuj Jana <sabujlovescoding@gmail.com>
2020-06-04 16:04:19 +0530

    Update Inorder Successor of a BST.cpp

--- Premium/Inorder Successor of a BST.cpp
+++ Premium/Inorder Successor of a BST.cpp
@@ -45,3 +45,51 @@ public:
         return succ;
     }
 };
+
+// Reverse inorder
+----------------------------------------------
+    /**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+typedef struct TreeNode node;
+class Solution {
+public:
+    /*
+     * @param root: The root of the BST.
+     * @param p: You need find the successor node of p.
+     * @return: Successor of p.
+     */
+    node* ans = NULL;
+    bool flag = false;
+    node* req = NULL;
+    
+    void inorder(node* root, node* p){
+        if(root == NULL)
+            return;
+            
+        if(flag)
+            return;
+            
+        cout << "hi" << endl;
+        inorder(root->right, p);
+        if(root == p){
+            req = ans;
+            flag = true;
+            return;
+        }
+        ans = root;
+        inorder(root->left, p);
+    }
+    TreeNode * inorderSuccessor(TreeNode * root, TreeNode * p) {
+        // write your code here
+        inorder(root, p);   
+        return req;
+    }
+};
