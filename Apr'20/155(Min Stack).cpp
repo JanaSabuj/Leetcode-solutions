23d5e1a1b7ca8abcc17755f0fc9bb3740c361c6a
Sabuj Jana <sabujlovescoding@gmail.com>
2020-06-10 13:01:40 +0530

    Update 155(Min Stack).cpp

--- Apr'20/155(Min Stack).cpp
+++ Apr'20/155(Min Stack).cpp
@@ -1,4 +1,67 @@
 class MinStack {
+    stack<long long int> s;
+    long long int mE;
+public:
+    /** initialize your data structure here. */
+    MinStack() {
+        
+    }
+    
+    void push(long long int x) {
+        if(s.empty()){
+            s.push(x);
+            mE = x;
+        }else{
+            if(x >= mE)
+                s.push(x);
+            else if(x < mE){
+                s.push(2 * x - mE);// flag insert
+                mE = x;// new mE
+            }
+        }
+    }
+    
+    void pop() {
+        if(s.empty())
+            return;
+        else{
+            if(s.top() >= mE)
+                s.pop();
+            else{
+                // flag
+                mE = 2 * mE - s.top();
+                s.pop();
+            }
+        }
+    }
+    
+    int top() {
+        if(s.empty())
+            return -1;
+        else if(s.top() >= mE)
+            return s.top();
+        else
+            return mE;        
+    }
+    
+    int getMin() {
+        if(s.empty())
+            return -1;
+        return mE;
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(x);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
+
+-----------------------------------------
+class MinStack {
     stack<int> s;
     stack<int> mix;
 public:
