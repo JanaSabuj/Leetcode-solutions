bf8084cc72cdbc41d15ac9623eea274f5aa00af1
Sabuj Jana <sabujlovescoding@gmail.com>
2020-07-30 12:08:30 +0530

    Create 146(LRU Cache).cpp

---
+++ Apr'20/146(LRU Cache).cpp
@@ -0,0 +1,94 @@
+class LRUCache {
+    int capacity;
+    
+    class node {
+    public:
+        int key;
+        int val;
+        node* next;
+        node* prev;
+    };
+    
+    unordered_map<int, node*> mp;
+    node* head;
+    node* tail;
+    
+    void removeNode(node* t){
+        node* prev = t->prev;
+        node* next = t->next;
+        
+        prev->next = next;
+        next->prev = prev;
+    }
+    
+    void addNode(node* t){
+        t->prev = head;
+        t->next = head->next;
+        
+        head->next->prev = t;
+        head->next = t;
+    }
+    
+    void moveNode(node* t){
+        this->removeNode(t);
+        this->addNode(t);
+    }
+    
+    node* popTail(){
+        node* last = tail->prev;
+        this->removeNode(last);
+        return last;
+    }
+    
+public:
+    LRUCache(int capacity) {
+        this->capacity = capacity;
+        head = new node();
+        tail = new node();
+        
+        head->next = tail;
+        head->prev = NULL;
+        
+        tail->next = NULL;
+        tail->prev = head;
+    }
+    
+    
+    
+    int get(int key) {
+        if(mp.find(key) == mp.end())
+            return -1;
+        
+        node* t = mp[key];
+        this->moveNode(t);
+        return t->val;
+    }
+    
+    void put(int key, int value) {
+        if(mp.find(key) != mp.end()){
+            // already available
+            node* t = mp[key];
+            t->val = value;
+            this->moveNode(t);
+        }else{
+            node* t = new node();
+            t->key = key;
+            t->val = value;
+            
+            this->addNode(t);
+            mp[key] = t;
+            
+            if(mp.size() > this->capacity){
+                node* last = this->popTail();
+                mp.erase(last->key);
+            }
+        }
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
