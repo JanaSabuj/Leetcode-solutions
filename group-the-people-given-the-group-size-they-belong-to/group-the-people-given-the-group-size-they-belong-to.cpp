2b36ac0f0b591c9bcd9256c7f5d6972684322de0
Sabuj Jana <sabujlovescoding@gmail.com>
2020-12-06 17:51:46 +0530

    working commit - Created using LeetHub

--- group-the-people-given-the-group-size-they-belong-to/group-the-people-given-the-group-size-they-belong-to.cpp
+++ group-the-people-given-the-group-size-they-belong-to/group-the-people-given-the-group-size-they-belong-to.cpp
@@ -4,27 +4,32 @@ public:
         int n = group.size();
         vector<vector<int>> vec(505);
         
+        vector<vector<int>> gs;
         for(int i = 0; i < n; i++){
             vec[group[i]].push_back(i);
+            if(vec[group[i]].size() == group[i]){
+                gs.push_back(vec[group[i]]);
+                vec[group[i]].clear();
+            }
         }
         
-        vector<vector<int>> gs;
         
-        for(int sz = 1; sz <= n; sz++){
-            if(vec[sz].size()){
-                int t = vec[sz].size();
-                vector<int> v;
+        
+//         for(int sz = 1; sz <= n; sz++){
+//             if(vec[sz].size()){
+//                 int t = vec[sz].size();
+//                 vector<int> v;
                 
-                for(int i = 1; i <= t; i++){
-                    v.push_back(vec[sz][i - 1]);
-                    if(i % sz == 0){                                                
-                        gs.push_back(v);
-                        v.clear();
-                    }                    
-                }
+//                 for(int i = 1; i <= t; i++){
+//                     v.push_back(vec[sz][i - 1]);
+//                     if(i % sz == 0){                                                
+//                         gs.push_back(v);
+//                         v.clear();
+//                     }                    
+//                 }
                 
-            }
-        }
+//             }
+//         }
         
         return gs;
     }
