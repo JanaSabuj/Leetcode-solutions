bd0f4067c259910fc1495eefd762dbc07279c11a
Sabuj Jana <sabujlovescoding@gmail.com>
2021-02-13 19:10:07 +0530

    Create 200.cpp

---
+++ 200/200.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int dx[4] = {0, 0, 1, -1};
+    int dy[4] = {1, -1, 0, 0};
+    
+    bool valid(int r, int c, int n, int m){
+        return (r >= 0 and r < n and c >= 0 and c < m);
+    }
+    
+    void dfs(vector<vector<char>>& grid, int i, int j, int n, int m){
+        grid[i][j] = '0';
+        for(int k = 0; k < 4; k++){
+            int r = i + dx[k];
+            int c = j + dy[k];
+            
+            if(valid(r, c, n, m) and grid[r][c] == '1')
+                dfs(grid, r, c, n, m);
+        }
+    }
+    
+    int numIslands(vector<vector<char>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        
+        int cnt = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(grid[i][j] == '1'){
+                    cnt++;
+                    dfs(grid, i, j, n, m);
+                }
+            }
+        }
+        
+        return cnt;
+    }
+};
