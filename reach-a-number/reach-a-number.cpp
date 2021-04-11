c0d70161a2c41aca287bd3ac9028df26fdb0e20b
Sabuj Jana <sabujlovescoding@gmail.com>
2021-04-11 12:18:11 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (60.71%) - LeetHub

--- reach-a-number/reach-a-number.cpp
+++ reach-a-number/reach-a-number.cpp
@@ -2,15 +2,16 @@ class Solution {
 public:
     int reachNumber(int target) {
         target = abs(target);
+        if(target == 0)
+            return 0;
+        
         int sum = 0;
         int idx = 1;
         
         while(1){
             sum += idx;
-            // cout << sum << endl;
-            if(sum == target)
-                return idx;            
-            if(sum > target and (sum - target) % 2 == 0)
+            // cout << sum << endl;                    
+            if(sum >= target and (sum - target) % 2 == 0)
                 return idx;
             idx++;
         }
