0ca504f57c7bb17c669e90745961630da20c0e9e
Sabuj Jana <sabujlovescoding@gmail.com>
2021-06-16 13:07:41 +0530

    Time: 32 ms (6.23%), Space: 28.1 MB (5.27%) - LeetHub

--- binary-tree-cameras/binary-tree-cameras.cpp
+++ binary-tree-cameras/binary-tree-cameras.cpp
@@ -9,49 +9,52 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-#define tn TreeNode* 
-#define inf (1<<20)
- 
- 
-map<pair<tn, pair<bool, bool>>, int> dp; 
- 
+#define inf (1 << 20)
+map<pair<TreeNode*, pair<int,int>>, int> dp;
+
 class Solution {
-    int solve(tn root, bool cam, bool parCam){
-        if(root == NULL){
-            if(cam)return inf;
-            else return 0;
-        }
-        if(root->left == NULL && root->right == NULL){
-            if(cam)
-                return 1;
-            else{
-                if(parCam)
-                    return 0;
-                else return inf;
-            }
-        }
- 
-        if(dp.find({root, {cam, parCam}}) != dp.end())
-            return dp[{root, {cam, parCam}}];
-        if(cam){
-            return dp[{root, {cam, parCam}}] = 1 + min(solve(root->left, 0, 1), solve(root->left, 1, 1)) +
-                        min(solve(root->right, 0, 1), solve(root->right, 1, 1));
-        }
-        else{
-            if(parCam){
-                return dp[{root, {cam, parCam}}] = min(solve(root->left, 0, 0), solve(root->left, 1, 0)) +
-                        min(solve(root->right, 0, 0), solve(root->right, 1, 0));
-            }
-            else{
-                int op1 = solve(root->left, 1, 0) + min(solve(root->right, 0, 0), solve(root->right, 1, 0));
-                int op2 = solve(root->right, 1, 0) + min(solve(root->left, 0, 0), solve(root->left, 1, 0));
-                return dp[{root, {cam, parCam}}] = min(op1, op2);
-            }
-        }
-    }
 public:
+    int solve(TreeNode* root, int placeCam, int parCam){
+        // base
+        if(root == NULL){
+            if(placeCam)
+                return inf;
+            else
+                return 0;    
+        }
+        
+        if(root->left == NULL and root->right == NULL){
+            if(placeCam)
+                return 1;
+            
+            if(parCam)
+                return 0;
+            else
+                return inf;
+        }
+        
+        // dp
+        if(dp.find({root, {placeCam, parCam}}) != dp.end())
+            return dp[{root, {placeCam, parCam}}];
+        
+        
+        // main
+        if(placeCam){
+            return dp[{root, {placeCam, parCam}}] = 1 + min(solve(root->left, 0, 1), solve(root->left, 1, 1)) + min(solve(root->right, 0, 1), solve(root->right, 1, 1));                         
+        }else {
+            if(parCam){
+                return dp[{root, {placeCam, parCam}}] = min(solve(root->left, 0, 0), solve(root->left, 1, 0)) + min(solve(root->right, 0, 0), solve(root->right, 1, 0));
+            }else{
+                int op1 = solve(root->left, 1, 0) + min(solve(root->right, 0, 0), solve(root->right, 1, 0));
+                
+                int op2 = solve(root->right, 1, 0) + min(solve(root->left, 0, 0), solve(root->left, 1, 0));
+                return dp[{root, {placeCam, parCam}}] = min( op1, op2);
+            }
+        }                
+    }
+    
     int minCameraCover(TreeNode* root) {
         dp.clear();
-        return min(solve(root, 0, 0), solve(root, 1, 0));
+        return min(solve(root, 0, 0), solve(root, 1, 0));        
     }
 };
