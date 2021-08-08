9a9331267e48c9a988131c24037f7b22f5083216
Sabuj Jana <sabujlovescoding@gmail.com>
2021-08-08 17:15:36 +0530

    Time: 4 ms (100.00%), Space: 6.9 MB (97.00%) - LeetHub

--- dice-roll-simulation/dice-roll-simulation.cpp
+++ dice-roll-simulation/dice-roll-simulation.cpp
@@ -1,42 +1,45 @@
 class Solution {
 public:
-    #define inf 1000000007
     int dieSimulator(int n, vector<int>& rollMax) {
-        // int dp[rolls][no][cnt]
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(6, vector<int>(16, 0)));
-        
-        // dp[r = 1][n][c]
-        for(int j = 0; j < 6; j++){
-            dp[0][j][1] = 1;
+        return getWays(n, rollMax);
+    }
+    
+    int getWays(int n, vector<int> maxRolls) {
+        // Code here
+        // dp[i][j] = no of seq till i throws and ending in j
+        const int mod = 1e9 + 7;
+        int dp[n + 1][8];
+
+        for (int i = 0; i < 6; i++) {
+            dp[1][i] = 1;
         }
-        
-        for(int i = 1; i < n; i++){
-            for(int j = 0; j < 6; j++){
+        dp[1][6] = 6;
+
+        for (int i = 2; i <= n; i++) {
+            int sum = dp[i - 1][6];// prev all sum
+            int tot = 0;
+            
+            for (int j = 0; j < 6; j++) {
+                int val = 0;
+                int prev_pos_till = i - maxRolls[j] - 1;
                 
-                // case 1 : dp[r][n][1]
-                for(int k = 0; k < 6; k++){
-                    if(k == j) continue;
-                    for(int m = 1; m <= rollMax[k]; m++){
-                        dp[i][j][1] = (dp[i][j][1] + dp[i - 1][k][m]) % inf;
-                    }
+                if (prev_pos_till < 0)
+                    val = val + 0;
+                else if (prev_pos_till == 0) {
+                    val = val + 1;
+                } else {
+                    val = val + (dp[prev_pos_till][6] - dp[prev_pos_till][j]) % mod;
                 }
-                
-                // case 2: dp[r][n][>=2]
-                for(int m = 2; m <= rollMax[j]; m++){
-                    dp[i][j][m] = dp[i - 1][j][m - 1];
-                }
+
+                int v = (sum - val) % mod;
+                v = (v + mod) % mod;
+                dp[i][j] = v;
+                tot = (tot + v) % mod;
             }
+
+            dp[i][6] = tot;
         }
-        
-        int cnt = 0;
-        for(int j = 0; j < 6; j++){
-            for(int k = 1; k <= rollMax[j]; k++){
-                cnt =  (cnt + dp[n - 1][j][k]) % inf;
-            }
-        }
-        
-        return cnt;
-     }
+
+        return dp[n][6];
+    }
 };
-// general house paint, no consecutive houses same color
-// dp[h][c] = sum(dp[h - 1][cc]) cc != c
