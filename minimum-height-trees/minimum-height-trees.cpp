991274394626dceedba5d06115053f2f65ea690f
Sabuj Jana <sabujlovescoding@gmail.com>
2021-10-16 17:00:06 +0530

    Time: 86 ms (49.66%), Space: 26.2 MB (94.67%) - LeetHub

--- minimum-height-trees/minimum-height-trees.cpp
+++ minimum-height-trees/minimum-height-trees.cpp
@@ -1,70 +1,40 @@
+typedef vector<int> vi;
 class Solution {
-
 public:
-    void dfs(int root, int par, vector<int> adj[], int ht1[], int ht2[]) {
+    vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
+        if(n == 1)
+            return {0};
         
-        for (auto v : adj[root]) {
-            if (v != par) {
-                dfs(v, root, adj, ht1, ht2);                
-            }
-        }
-        
-        int hta = -1, htb = -1;
-        for(auto v: adj[root]){
-            if(v != par){
-                int tmp = 1 + ht1[v];
-                if (tmp > hta)
-                {
-                    htb = hta;
-                    hta = tmp;
-                } else if (tmp >= htb) {
-                    htb = tmp;
-                }
-            }
-        }
-
-        ht1[root] = max(hta, 0);
-        ht2[root] = htb;       
-    }
-    
-    void dfs2(int root, int par, vector<int> adj[], int ht1[], int ht2[], int ht3[], int acc){        
-         ht3[root] = max(acc, ht1[root]);
-        
-         for (auto v : adj[root]) {             
-            if (v != par) {
-                if(1 + ht1[v] == ht1[root]){
-                    dfs2(v, root, adj, ht1, ht2, ht3, max(1 + ht2[root], 1 + acc));                    
-                }else {
-                dfs2(v, root, adj, ht1, ht2, ht3, max(1 + ht1[root], 1 + acc));                    
-                }
-            }
-        }
-    }
-
-    vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {        
-        vector<int> adj[n];
-
-        for (auto e : edges) {
+        vi adj[n];
+        vi indeg(n,0);
+        for(auto& e: edges){
             adj[e[0]].push_back(e[1]);
             adj[e[1]].push_back(e[0]);
+            indeg[e[0]]++;
+            indeg[e[1]]++;
         }
 
-        int ht1[n], ht2[n], ht3[n];
-        dfs(0, -1, adj, ht1, ht2);
-        
-        dfs2(0, -1, adj, ht1, ht2, ht3, 0);
-
-        vector<int> ans;
-        int mn = INT_MAX;
-        
-        for (int i = 0; i < n; ++i) {
-            cout << ht1[i] << " " << ht2[i] << " " << ht3[i] << endl;            
-            mn = min(mn, ht3[i]);
+        // topo - kahn's
+        queue<int> q;
+        for (int i = 0; i < n; ++i){
+            if(indeg[i] == 1)
+                q.push(i);
         }
 
-        for (int i = 0; i < n; ++i) {
-            if (ht3[i] == mn)
-                ans.push_back(i);
+        vi ans;
+        while(!q.empty()){
+            int sz = q.size();
+            ans.clear();
+            while(sz--){
+                int curr = q.front();
+                q.pop();
+                ans.push_back(curr);
+                for(auto v: adj[curr]){
+                    indeg[v]--;
+                    if(indeg[v] == 1)
+                        q.push(v);
+                }
+            }
         }
 
         return ans;
