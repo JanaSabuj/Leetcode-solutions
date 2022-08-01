58904fbe2ccc3c72ec8a4ad7b394ce9c1012cb7b
Sabuj Jana <sabujlovescoding@gmail.com>
2022-08-01 19:34:52 +0530

    Time: 7 ms (98.28%), Space: 10.6 MB (57.65%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,22 +1,52 @@
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//         int n = nums.size();
+//         unordered_map<int,int> mp;
+//         for(int i = 0; i < n; i++) {
+//             // 2   t = 7
+//             // search (7 - 2)
+//             if(mp.count(target - nums[i])) {
+//                 // found ans
+//                 return {i, mp[target - nums[i]]};
+//             }
+
+//             mp[nums[i]] = i;
+//         }
+
+//         return {};
+//     }
+// };
+
+// TC: O(N) - one iteration
+// SC: O(N) - wc scenario, all elements will be in hashmap
+
+using pii = pair<int,int>;
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int n = nums.size();
-        unordered_map<int,int> mp;
+        vector<pii> vec(n);
         for(int i = 0; i < n; i++) {
-            // 2   t = 7
-            // search (7 - 2)
-            if(mp.count(target - nums[i])) {
-                // found ans
-                return {i, mp[target - nums[i]]};
-            }
+            vec[i] = {nums[i], i};
+        }
+        sort(vec.begin(), vec.end());
+        // (1,4) (2,1) (5,2) (7,3) (9,0)
 
-            mp[nums[i]] = i;
+        int i = 0, j = n - 1;
+        while(i < j) {
+            if(vec[i].first + vec[j].first == target)
+                return {vec[i].second, vec[j].second};
+            else if(vec[i].first + vec[j].first < target)
+                i++;
+            else
+                j--;
         }
 
         return {};
     }
 };
 
-// TC: O(N) - one iteration
-// SC: O(N) - wc scenario, all elements will be in hashmap
+// 2P method hint because : a + b = target and array can be SORTED
+// TC: O(NlogN) - sorting the array, then iterating
+// SC: O(N) - storing N pairs
